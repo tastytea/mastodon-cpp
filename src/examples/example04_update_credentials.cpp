6cb9589cc0af352ce1432fbdc014c0f16c02dc53
tastytea <tastytea@tastytea.de>
2018-03-21 20:12:45 +0100

    fixed header searching

--- src/examples/example04_update_credentials.cpp
+++ src/examples/example04_update_credentials.cpp
@@ -6,7 +6,11 @@
 #include <vector>
 #include <string>
 #include <cstdint>
-#include "mastodon-cpp/mastodon-cpp.hpp"
+#ifdef MASTODON_CPP
+    #include "mastodon-cpp.hpp"
+#else
+    #include <mastodon-cpp/mastodon-cpp.hpp>
+#endif
 
 using Mastodon::API;
 

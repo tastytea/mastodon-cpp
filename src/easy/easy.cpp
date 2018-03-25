b6bb41e93acc07913d660a29eb00a99e0c8ed944
tastytea <tastytea@tastytea.de>
2018-03-25 22:46:38 +0200

    refactored Easy::Entity and children

--- src/easy/easy.cpp
+++ src/easy/easy.cpp
@@ -16,11 +16,19 @@
 
 #include <string>
 #include <sstream>
+#include <cstdint>
+#include <chrono>
+#include <ctime>
+#include <iomanip>  // get_time
+#include <sstream>
+#include <jsoncpp/json/json.h>
 #include "easy.hpp"
 #include "macros.hpp"
 
 using namespace Mastodon;
 using std::string;
+using std::uint64_t;
+using std::chrono::system_clock;
 
 Easy::Easy(const string &instance, const string &access_token)
 : API(instance, access_token)
@@ -48,4 +56,109 @@ Easy::Entity::Entity(const string &json)
 const bool Easy::Entity::valid() const
 {
     return _valid;
+}
+
+const Json::Value Easy::Entity::get(const string &key) const
+{
+    const Json::Value *node;
+    if (key.find('.') == std::string::npos)
+    {
+        node = &_tree[key];
+    }
+    else
+    {
+        // If dots in key, we have to walk through the tree
+        std::size_t pos = 0;
+        string current_key = key;
+        node = &_tree;
+        while ((pos = current_key.find('.')) != std::string::npos)
+        {
+            try
+            {
+                node = &(*node)[current_key.substr(0, pos)];
+                current_key = current_key.substr(pos + 1);
+            }
+            catch (const Json::LogicError &e)
+            {
+                ttdebug << e.what() << '\n';
+                goto error;
+            }
+        }
+        node = &(*node)[current_key];
+    }
+
+    if (!node->isNull())
+    {
+        return *node;
+    }
+
+    error:
+    ttdebug << "Could not get data: " << key << '\n';
+    return Json::Value();
+}
+
+const string Easy::Entity::get_string(const string &key) const
+{
+    const Json::Value node = get(key);
+
+    if (node.isString())
+    {
+        return node.asString();
+    }
+
+    return "";
+}
+
+const uint64_t Easy::Entity::get_uint64(const string &key) const
+{
+    const Json::Value node = get(key);
+
+    if (node.isUInt64())
+    {
+        return node.asUInt64();
+    }
+
+    return 0;
+}
+
+const double Easy::Entity::get_double(const string &key) const
+{
+    const Json::Value node = get(key);
+
+    if (node.isDouble())
+    {
+        return node.asDouble();
+    }
+
+    return 0.0;
+}
+
+const bool Easy::Entity::get_bool(const string &key) const
+{
+    const Json::Value node = get(key);
+
+    if (node.isBool())
+    {
+        return node.asBool();
+    }
+
+    return false;
+}
+
+const system_clock::time_point
+    Easy::Entity::get_time_point(const string &key) const
+{
+    const Json::Value node = get(key);
+
+    if (node.isString())
+    {
+        std::stringstream sstime(node.asString());
+        struct std::tm tm = {0};
+        sstime >> std::get_time(&tm, "%Y-%m-%dT%T");
+        std::time_t time = timegm(&tm);
+        return system_clock::from_time_t(time);
+    }
+
+    // Return clocks epoch
+    return system_clock::time_point();
 }
--- src/easy/easy.hpp
+++ src/easy/easy.hpp
@@ -18,6 +18,8 @@
 #define MASTODON_EASY_CPP_HPP
 
 #include <string>
+#include <cstdint>
+#include <chrono>
 #include <jsoncpp/json/json.h>
 
 // If we are compiling mastodon-cpp, use another include path
@@ -28,6 +30,8 @@
 #endif
 
 using std::string;
+using std::uint64_t;
+using std::chrono::system_clock;
 
 namespace Mastodon
 {
@@ -92,12 +96,60 @@ public:
     class Entity
     {
     public:
-        explicit Entity(const string &json);
+        /*!
+         *  @brief  Constructs an Entity object from a JSON string.
+         *
+         *  @param  json    JSON string
+         */
+        Entity(const string &json);
 
         /*!
          *  @brief  Returns true if the Entity holds valid data
          */
         const bool valid() const;
+
+        /*!
+         *  @brief  Returns the value of node as Json::Value
+         *  
+         *          Returns an empty object on error.
+         */
+        const Json::Value get(const string &key) const;
+
+        /*!
+         *  @brief  Returns the value of node as std::string
+         *  
+         *          returns "" on error.
+         */
+        const string get_string(const string &key) const;
+
+        /*!
+         *  @brief  Returns the value of node as std::uint64_t
+         *  
+         *          Returns 0 on error.
+         */
+        const uint64_t get_uint64(const string &key) const;
+
+        /*!
+         *  @brief  Returns the value of node as double
+         *  
+         *          Returns 0.0 on error.
+         */
+        const double get_double(const string &key) const;
+
+        // TODO: Investigate if uint8_t would be better
+        /*!
+         *  @brief  Returns the value of node as bool
+         *  
+         *          Returns false on error.
+         */
+        const bool get_bool(const string &key) const;
+
+        /*!
+         *  @brief  Returns the value of node as time_point
+         *  
+         *          Returns clocks epoch on error.
+         */
+        const system_clock::time_point get_time_point(const string &key) const;
 
     protected:
         Json::Value _tree;

36279a26f47900654dc217148f39e8b8a50ac350
tastytea <tastytea@tastytea.de>
2018-06-07 21:46:49 +0200

    ...

--- build_doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f Doxyfile ]; then
-    mkdir -p doc
-    (cat Doxyfile && echo -n "PROJECT_NUMBER = " &&
-       grep -Eo '[0-9]+.[0-9]+.[0-9]+$' CMakeLists.txt) \
-       | doxygen -
-fi

43fd2aae6e279aab4cf9610025d8ea0dbcfe2689
tastytea <tastytea@tastytea.de>
2019-03-30 20:53:53 +0100

    Changed debug.hpp to respect 80 char rule.

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,6 +1,6 @@
 /*  This file is part of mastodon-cpp.
- *  Copyright © 2018 tastytea <tastytea@tastytea.de>
- *                                                                   
+ *  Copyright © 2018, 2019 tastytea <tastytea@tastytea.de>
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, version 3.
@@ -20,7 +20,8 @@
 #include <iostream>
 
 #ifdef DEBUG
-    #define ttdebug std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] DEBUG: "
+    #define ttdebug \
+            std::cerr << "[" << __FILE__ << ":" << __LINE__ << "] DEBUG: "
 #else
     #define ttdebug false && std::cerr
 #endif

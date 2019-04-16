984f40a7a48b45ee6cdba77cdb215474d04ee865
tastytea <tastytea@tastytea.de>
2019-04-16 03:25:44 +0200

    Added tests for API::v1::accounts_id_block.

---
+++ tests/test_api_v1_accounts_id_block.cpp
@@ -0,0 +1,79 @@
+/*  This file is part of mastodon-cpp.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <exception>
+#include <string>
+#include <cstdlib>
+#include <catch.hpp>
+#include "mastodon-cpp.hpp"
+#include "easy/easy.hpp"
+#include "easy/entities/relationship.hpp"
+
+using namespace Mastodon;
+
+SCENARIO ("/api/v1/accounts/:id/blocks can be called successfully",
+          "[api][mastodon][pleroma][glitch-soc]")
+{
+    const char *env_instance = std::getenv("MASTODON_CPP_INSTANCE");
+    const string instance = (env_instance ? env_instance : "likeable.space");
+    const char *access_token = std::getenv("MASTODON_CPP_ACCESS_TOKEN");
+    const char *env_user_id = std::getenv("MASTODON_CPP_USER_ID");
+    const string user_id = (env_user_id ? env_user_id : "9hnrrVPriLiLVAhfVo");
+
+    REQUIRE (access_token != nullptr);
+
+    GIVEN ("instance = " + instance)
+    {
+        Mastodon::Easy::API masto(instance, access_token);
+        return_call ret;
+        Easy::Relationship relationship;
+        bool exception = false;
+
+        WHEN ("/api/v1/accounts/" + user_id + "/block is called")
+        {
+            try
+            {
+                ret = masto.post(API::v1::accounts_id_block,
+                                 {
+                                     { "id", { user_id }}
+                                 });
+                relationship.from_string(ret.answer);
+            }
+            catch (const std::exception &e)
+            {
+                exception = true;
+                WARN(e.what());
+            }
+
+            THEN("No exception is thrown")
+                AND_THEN ("No errors are returned")
+                AND_THEN ("Answer is valid")
+                AND_THEN ("The answer makes sense")
+            {
+                REQUIRE_FALSE(exception);
+
+                // You can't block yourself, so we look for errors too.
+                REQUIRE((ret.error_code == 0
+                         || ret.error_code == 111));
+                REQUIRE((ret.http_error_code == 200
+                         || ret.http_error_code == 500));
+
+                REQUIRE((relationship.blocking()
+                         || relationship.error() != ""));
+            }
+        }
+    }
+}

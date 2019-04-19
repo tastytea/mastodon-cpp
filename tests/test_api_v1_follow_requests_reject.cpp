8094603c0f04586e5f805d94c6e428c1b5a31d25
tastytea <tastytea@tastytea.de>
2019-04-20 00:19:31 +0200

    Added tests for API::v1::follow_requests_id_reject.

---
+++ tests/test_api_v1_follow_requests_reject.cpp
@@ -0,0 +1,64 @@
+/*  This file is part of mastodon-cpp.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <exception>
+#include <string>
+#include <catch.hpp>
+#include "mastodon-cpp.hpp"
+#include "easy/easy.hpp"
+#include "environment_variables.hpp"
+
+using namespace Mastodon;
+
+SCENARIO ("/api/v1/follow_requests/:id/reject can be called successfully",
+          "[api][mastodon][pleroma][glitch-soc]")
+{
+    REQUIRE (access_token != nullptr);
+
+    GIVEN ("instance = " + instance)
+    {
+        Mastodon::Easy::API masto(instance, access_token);
+        return_call ret;
+        bool exception = false;
+
+        WHEN ("POST /api/v1/follow_requests/abc123/reject is called")
+        {
+            try
+            {
+                ret = masto.post(API::v1::follow_requests_id_reject,
+                                 {{ "id", { "abc123" }}});
+            }
+            catch (const std::exception &e)
+            {
+                exception = true;
+                WARN(e.what());
+            }
+
+            THEN("No exception is thrown")
+                AND_THEN ("No unexpected errors are returned")
+                AND_THEN ("The answer makes sense")
+            {
+                REQUIRE_FALSE(exception);
+
+                REQUIRE(ret.error_code == 111);
+                REQUIRE(ret.http_error_code == 404);
+
+                REQUIRE(ret.answer.substr(0, 5) == "{\"err");
+            }
+        }
+    }
+}
+

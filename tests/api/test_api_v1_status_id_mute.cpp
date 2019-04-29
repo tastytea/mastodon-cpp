95b9dd868dab9a18f7380e313c3be6b73da5ef9b
tastytea <tastytea@tastytea.de>
2019-04-29 18:25:43 +0200

    Added tests for API::v1::statuses_id_mute.

---
+++ tests/api/test_api_v1_status_id_mute.cpp
@@ -0,0 +1,72 @@
+/*  This file is part of mastodon-cpp.
+ *  Copyright © 2019 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <exception>
+#include <string>
+#include <catch.hpp>
+#include "mastodon-cpp.hpp"
+#include "easy/easy.hpp"
+#include "easy/entities/status.hpp"
+#include "../environment_variables.hpp"
+
+using namespace Mastodon;
+
+SCENARIO ("/api/v1/statuses/:id/mute can be called successfully",
+          "[api][auth][mastodon][glitch-soc]")
+{
+    REQUIRE (access_token != nullptr);
+
+    GIVEN ("instance = " + instance)
+    {
+        Mastodon::Easy::API masto(instance, access_token);
+        return_call ret;
+        Easy::Status status;
+        bool exception = false;
+
+        WHEN ("POST /api/v1/statuses/" + status_id + "/mute is called")
+        {
+            try
+            {
+                ret = masto.post(API::v1::statuses_id_mute,
+                                 {
+                                     { "id", { user_id } }
+                                 });
+                status.from_string(ret);
+            }
+            catch (const std::exception &e)
+            {
+                exception = true;
+                WARN(e.what());
+            }
+
+            THEN("No exception is thrown")
+                AND_THEN ("No unexpected errors are returned")
+                AND_THEN ("Answer is valid")
+                AND_THEN ("The answer makes sense")
+            {
+                REQUIRE_FALSE(exception);
+
+                REQUIRE(ret.error_code == 0);
+                REQUIRE(ret.http_error_code == 200);
+
+                REQUIRE(status.valid());
+
+                REQUIRE(status.muted());
+            }
+        }
+    }
+}
+
